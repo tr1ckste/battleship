3758b380f9848e153db66492586b769166264c43
tr1ckste <nikitavasilinenko@gmail.com>
2021-01-09 13:57:04 +0200

    fixed recursion

--- static/index.js
+++ static/index.js
@@ -2,32 +2,50 @@
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const margin = 10;
 
-const url = 'screens/initial.js';
-const getScreen = url => {
-  fetch(url, {
+const getScreen = async url => {
+  const response = await fetch(url, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
     }
-  }).then(screen => screen.json()
-    .then(screen => {
-      ctx.clearRect(0, 0, 900, 600);
-      const { instructions, buttons } = screen;
-      for (const instruction of instructions) {
-        const comm = comms[instruction.name];
-        comm(...instruction.args);
-      }
-      canvas.addEventListener('click', event => {
-        const { clientX, clientY } = event;
-        for (const button of buttons) {
-          const { x1, y1, x2, y2 } = button.coords;
-          const condition1 = clientX < x2 && clientX > x1;
-          const condition2 = clientY < y2 && clientY > y1;
-          if (condition1 && condition2) getScreen(button.url);
-        }
-      });
-    }));
+  });
+  return await response.json();
 };
 
-getScreen(url);
+const url = 'screens/initial.js';
+let SCREEN = getScreen(url);
+
+const renderScreen = screen => {
+  ctx.clearRect(0, 0, 900, 600);
+  const { instructions } = screen;
+  for (const instruction of instructions) {
+    const comm = comms[instruction.name];
+    comm(...instruction.args);
+  }
+};
+
+canvas.addEventListener('click', event => {
+  const { clientX, clientY } = event;
+  const { buttons } = SCREEN;
+  const cnvX = clientX - margin;
+  const cnvY = clientY - margin;
+
+  for (const button of buttons) {
+    const { x1, y1, x2, y2 } = button.coords;
+    const condition1 = cnvX < x2 && cnvX > x1;
+    const condition2 = cnvY < y2 && cnvY > y1;
+    if (condition1 && condition2) {
+      getScreen(button.url).then(screen => {
+        SCREEN = screen;
+        renderScreen(SCREEN);
+      });
+    }
+  }
+});
+
+SCREEN.then(screen => {
+  SCREEN = screen;
+  renderScreen(SCREEN);
+});
